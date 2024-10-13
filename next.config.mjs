324c8433a37367309d94e8ed82e9548abc7dd56a
Austin Steeno <austin.steeno@gmail.com>
2024-10-13 16:55:06 -0700

    Clean up and add support for importing HTML markup as text

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export"
+    output: "export",
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.html$/i,
+            loader: "html-loader"
+        });
+
+        // Important: return the modified config
+        return config;
+    }
 };
 
 export default nextConfig;
